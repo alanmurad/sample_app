ce1da888dc34f75ff20e9c6247d036b3ecd4281f
alanmurad <alan.murad42@gmail.com>
2020-06-13 11:51:27 +0100

    Added contact page, and a adjusted spec test for routing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,17 +6,17 @@ describe "Static Pages" do
 
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')   
     end
 
     it "should have the base title" do
-  		visit '/static_pages/home'
+  		visit root_path
   		expect(page).to have_title("Ruby on Rails Tutorial Sample App")
   	end
 
   	it "should not have a custom page title" do
-  		visit '/static_pages/home'
+  		visit root_path
   		expect(page).not_to have_title('| Home')
   	end
   end
@@ -24,12 +24,12 @@ describe "Static Pages" do
   describe "Help page" do 
   	
   	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		expect(page).to have_content('Help') 
   	end
 
   	it "should have the right title" do
-	  	visit '/static_pages/help'
+	  	visit help_path
 	  	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
   	end
   end
@@ -37,13 +37,25 @@ describe "Static Pages" do
   describe "About page" do
   	
   	it "should have the content 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		expect(page).to have_content('About Us')
   	end
   	
   	it "should have the right title" do
-	  	visit '/static_pages/about'
+	  	visit about_path
 	  	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
 	  end
+  end
+  describe "Contact page" do
+
+    it "should have the cotnent 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit contact_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
   end
 end
