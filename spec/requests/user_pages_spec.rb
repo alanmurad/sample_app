febc300e5294211916ff9703ffef30b7a05a88c2
alanmurad <alan.murad42@gmail.com>
2020-06-15 12:44:52 +0100

    Added Factory gem, tested user show page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,7 +4,15 @@ describe "UserPages" do
   
   subject { page }
 
-  describe "GET /user_pages" do
+  describe "profile page" do
+  	let(:user) { FactoryGirl.create(:user) }
+  	before { visit user_path(user) }
+
+  	it { should have_content(user.name) }
+  	it { should have_title(user.name) }
+  end
+
+  describe "signup page" do
     before { visit signup_path }
 
     it { should have_content('Sign up') }
